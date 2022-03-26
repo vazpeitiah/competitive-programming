67a5f466e9dbd19bdfabb0dead2d1ff981130b71
vazpeitiah <vazpeitiah@gmail.com>
2022-03-26 13:03:26 -0600

    codeforces 1399B

---
+++ codeforces/problemset/1399B - Gifts Fixing.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  int t, n, minb, mina;
+  long long movs;
+  cin >> t;
+  while(t-- > 0) {
+    cin >> n;
+    minb = INT_MAX;
+    mina = INT_MAX;
+    vector<int> a(n);
+    vector<int> b(n);
+    movs=0;
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+      mina = min(mina, a[i]);
+    }
+    for (int i = 0; i < n; i++) {
+      cin >> b[i];
+      minb = min(minb, b[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+      movs += a[i] - mina;
+      movs += b[i] - minb;
+
+      if(a[i] > mina && b[i] > minb) {
+        if(min(a[i] - mina, b[i] - minb) == a[i] - mina) {
+          movs -= a[i] - mina;
+        } else {
+          movs -= b[i] - minb;
+        }
+      }
+    }
+
+    cout << movs << endl;
+  }
+  return 0;
+}
