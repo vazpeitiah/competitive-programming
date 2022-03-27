3341f0dc1343bfb94be92f039e77fae3122b1a30
vazpeitiah <vazpeitiah@gmail.com>
2022-03-27 10:50:55 -0600

    printing pattern with c

---
+++ hackerrank/c/printing_patterns.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern. for n
+                         /* 4 4 4 4 4 4 4  
+                            4 3 3 3 3 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 2 1 2 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 3 3 3 3 4   
+                            4 4 4 4 4 4 4   */
+    int i, j, min;
+    int len = 2*n - 1;
+    for(i=0; i<len; i++) {
+        for(j=0; j<len; j++) {
+            min = i < j ? i : j;
+            min = min < len-i ? min : len-i-1;
+            min = min < len-j-1 ? min : len-j-1;
+            printf("%d ", n-min);
+        }
+        printf("\n");
+    }
+    return 0;
+}
