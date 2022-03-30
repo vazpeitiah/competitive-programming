e9a9b1cc6e6660e0dc7977ff3a97a0565c113e0c
vazpeitiah <vazpeitiah@gmail.com>
2022-03-30 08:12:25 -0600

    codeforces 1551A

---
+++ codeforces/problemset/1551A - Polycarp and Coins.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  int t, n;
+  cin >> t;
+  while(t--) {
+    cin >> n;
+
+    if(n%3 == 0) {
+      cout << n/3 << " " << n/3 << endl;
+    } else if(n%3 == 1){
+      cout << n/3+1 << " " << n/3 << endl;
+    } else {
+      cout << n/3 << " " << n/3+1 << endl;
+    }
+
+
+  }
+  return 0;
+}
