2be4b0c5e1f017bb1a6b5ec1e1a21c0c3d4cb8bf
vazpeitiah <vazpeitiah@gmail.com>
2022-04-11 10:48:57 -0500

    Omegaup 11232 Frecuencia de una letra

---
+++ omegaup/easy/Omegaup_11232.java
@@ -0,0 +1,33 @@
+package omegaup.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Omegaup_11232 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String input;
+    input = scan.nextLine();
+    
+    String ss[] = input.split(" ");
+
+    String word = ss[0];
+    char c = ss[1].charAt(0);
+
+    Map<Character, Integer> map = new HashMap<>();
+    
+    for(int i=0; i<word.length(); i++) {
+      char key = word.charAt(i);
+      if(!map.containsKey(key)) {
+        map.put(key, 1);
+      } else {
+        map.put(key, map.get(key) + 1);
+      }
+    }
+
+    System.out.print(map.get(c));
+
+    scan.close();
+  } 
+}
