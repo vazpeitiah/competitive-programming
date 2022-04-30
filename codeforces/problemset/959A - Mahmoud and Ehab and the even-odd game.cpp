af7ca6c74f03cfcef461f6ba4de1c1eae7c7a42a
vazpeitiah <vazpeitiah@gmail.com>
2022-04-30 09:36:54 -0500

    codeforces 959A

---
+++ codeforces/problemset/959A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  int n; cin >> n;
+  cout << (n%2==0 ? "Mahmoud" : "Ehab") << endl;
+  return 0;
+}
