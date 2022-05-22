5f16dce6b7d9a5a939026c055843c3b6c0450c3a
vazpeitiah <vazpeitiah@gmail.com>
2022-05-22 08:39:14 -0500

    codeforces 372A

---
+++ codeforces/problemset/372A - Omkar and Completion.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  int t; cin >> t;
+  int n;
+  while(t--) {
+    cin >> n;
+    for(int i=0; i<n; i++) cout << "1" << " ";
+    cout << endl;
+  }
+  return 0;
+}
