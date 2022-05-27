1b2263385073de4cc176169dc4404476802b5c4c
vazpeitiah <vazpeitiah@gmail.com>
2022-05-27 08:03:21 -0500

    codeforces 1325A

---
+++ codeforces/problemset/1325A - EhAb AnD gCd.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+  int t; cin >> t;
+  int x;
+  while(t--) {
+    cin >> x;
+    cout << 1 << " " << x-1 << endl;
+  }
+  return 0;
+}
