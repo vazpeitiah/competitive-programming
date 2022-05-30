97326cd4fa538efaade06d9d6deaca67e492acbf
Vladimir Azpeitia <vazpeitiah@gmail.com>
2022-05-30 08:33:25 -0500

    codeforces 742A

---
+++ codeforces/problemset/742A - Arpa's hard exam and Mehrdads naive cheat.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+  int n; cin >> n;
+  if(n==0) cout << 1 << endl;
+  else  if(n%4 == 0) cout << 6 << endl;
+  else if(n%4 == 1) cout << 8 << endl;
+  else if(n%4 == 2) cout << 4 << endl;
+  else cout << 2 << endl;
+
+  return 0;
+}
